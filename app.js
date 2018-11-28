5d94386d7b923a071377750484189d03b76cf2f5
Cesar Garcia <cesar.garcia.acoltzi@gmail.com>
2018-11-27 22:57:08 -0600

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
 
 // Inicializar variables
 var app = express();
 
+// Body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuariosRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
 // Conexión a las base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
 
-  if ( err ) throw err;
+    if (err) throw err;
 
-  console.log('Base de datos, \x1b[32m%s\x1b[0m', 'Listo!');
+    console.log('Base de datos, \x1b[32m%s\x1b[0m', 'Listo!');
 
 });
 
 // Rutas
-app.get('/', (req, res, next) => {
-
-  res.status(200).json({
-    ok: true,
-    mensaje: 'Petición realizada correctamente'
-  });
-
-});
+app.use('/usuario', usuariosRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 // Escuchar peticiones
 app.listen(3000, () => {
-  console.log('Express Server, puesto: 3000: \x1b[32m%s\x1b[0m', 'Listo!');
+    console.log('Express Server, puesto: 3000: \x1b[32m%s\x1b[0m', 'Listo!');
 });
